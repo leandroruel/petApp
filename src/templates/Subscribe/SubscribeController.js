ab12b4a89b7672fa4ae96f8d410bf5a1a1767d60
Leandro Ruel <leandro.ruel@gmail.com>
2018-11-13 14:09:15 -0200

    add form submit to subscribe, add link with params to detail button

--- src/templates/Subscribe/SubscribeController.js
+++ src/templates/Subscribe/SubscribeController.js
@@ -4,8 +4,32 @@ angular
 .module('petApp')
 .controller('SubscribeController', SubscribeController)
 
-SubscribeController.$inject = ['$scope'];
+SubscribeController.$inject = ['$scope', '$document', '$stateParams', '$state'];
 
-function SubscribeController ($scope) {
-    
+/**
+ * set the controller for the subscribe form
+ * 
+ * @param {*} $scope 
+ */
+function SubscribeController ($scope, $document, $stateParams, $state) {
+    // get the form button
+    const BtnSubscribe = angular.element( document.querySelector("#subscribe") );
+
+    // get pet id from url
+    $scope.petId = $stateParams.id;
+    // get data from ng-model
+    $scope.user = {
+        name: '',
+        address: '',
+        address2: '',
+        reason: ''
+    };
+
+    BtnSubscribe.on('click', () => {        
+        console.log($scope.user)
+    })
+
+    $scope.submitForm = function () {
+        $state.go('/subscribe/finish', { id: $stateParams.id, name: $scope.user.name})
+    }
 }
