01d324aecc57d6517f915454d0006fe41ca79c57
Leandro Ruel <leandro.ruel@gmail.com>
2018-11-13 14:02:49 -0200

    add import of style.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular'
 import uirouter from '@uirouter/angularjs'
 import 'bootstrap/dist/css/bootstrap.min.css'
+require('src/scss/style.scss');
 
 var app = angular.module('petApp', [uirouter]);
 
