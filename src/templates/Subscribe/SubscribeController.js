f79e3b8613e0b76d110a7799c67c87fd5c5a781f
Leandro Ruel <leandro.ruel@gmail.com>
2018-11-13 03:04:04 -0200

    adiciona controller e renomeia a view

---
+++ src/templates/Subscribe/SubscribeController.js
@@ -0,0 +1,11 @@
+import angular from 'angular'
+
+angular
+.module('petApp')
+.controller('SubscribeController', SubscribeController)
+
+SubscribeController.$inject = ['$scope'];
+
+function SubscribeController ($scope) {
+    
+}
